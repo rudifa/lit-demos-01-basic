94234ab5e48fe32faa6dac93fd899bc6c43c67b1
Rudi Farkas <rudi.farkas@gmail.com>
2020-04-15 14:34:33 +0200

    firing-events: add click count transmission child=>parent

--- src/12-firing-events.js
+++ src/12-firing-events.js
@@ -3,20 +3,43 @@
 import { LitElement, html, css } from 'lit-element';
 
 class FireEventsParent extends LitElement {
+  static get properties() {
+    return {
+      _messageFromChild: { type: String },
+    }
+  }
+  constructor() {
+    super()
+    this._messageFromChild = ""
+  }
+
   someCallback(event) {
-    console.log(event.detail);
+    //console.log(event.detail);
+    this._messageFromChild = event.detail
   }
 
   render() {
     return html`
       <fire-events-child @event-fired=${this.someCallback}></fire-events-child>
+      <p>${this._messageFromChild}</p>
     `;
   }
 }
 
 class FireEventsChild extends LitElement {
+  static get properties() {
+    return {
+      _messageCount: { type: Number },
+    }
+  }
+  constructor() {
+    super()
+    this._messageCount = 0
+  }
+
   handleClick() {
-    this.dispatchEvent(new CustomEvent('event-fired', { detail: "custom event fired in child" }));
+    this._messageCount++
+    this.dispatchEvent(new CustomEvent('event-fired', { detail: `custom event ${this._messageCount} fired in child` }));
   }
 
   render() {
